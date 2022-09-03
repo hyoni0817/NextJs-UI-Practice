2ec69854030f6ae235c63c01521500ce01c41850
hyoni0817 <hyoni0817@naver.com>
2022-09-04 01:18:37 +0900

    Set: 화살표 함수로 컴포넌트를 선언할 수 있도록 eslint 룰 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/function-component-definition': 0,
   },
   settings: {
     react: {
