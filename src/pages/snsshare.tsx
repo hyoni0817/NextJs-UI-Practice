55b2563bff41ace29f740bb16ab9358ab6022bca
hyoni0817 <hyoni0817@naver.com>
2022-09-23 23:57:30 +0900

    Feat: SNS 공유 페이지에 타이틀 추가

---
+++ src/pages/snsshare.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const SnsShare = () => <Title>SNS 공유하기</Title>;
+
+export default SnsShare;
