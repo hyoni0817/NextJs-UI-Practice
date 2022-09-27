a8d4bd69d7567aa53452f84d1a74938f64f39664
hyoni0817 <hyoni0817@naver.com>
2022-09-27 23:44:10 +0900

    Feat: 인피니트 스크롤을 구현할 페이지 생성

---
+++ src/pages/infiniteScroll.tsx
@@ -0,0 +1,10 @@
+// IntersectionObserver로 무한 스크롤링 구현하기
+import React from 'react';
+
+const infiniteScroll = () => (
+  <div>
+    <h1>사랑과 관련된 모든 책 모음</h1>
+  </div>
+);
+
+export default infiniteScroll;
