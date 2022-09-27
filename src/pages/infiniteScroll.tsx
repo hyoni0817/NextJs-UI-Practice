ed780648c0ff36179c20454255580a4400f02960
hyoni0817 <hyoni0817@naver.com>
2022-09-28 01:15:41 +0900

    Feat: 제목에 사랑이라는 단어가 포함된 책 리스트를 로드할 수 있게 구현

--- src/pages/infiniteScroll.tsx
+++ src/pages/infiniteScroll.tsx
@@ -1,10 +1,61 @@
 // IntersectionObserver로 무한 스크롤링 구현하기
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const infiniteScroll = () => (
-  <div>
-    <h1>사랑과 관련된 모든 책 모음</h1>
-  </div>
-);
+type BookListType = {
+  author: string;
+  description: string;
+  discount: string;
+  image: string;
+  isbn: string;
+  link: string;
+  pubdate: string;
+  publisher: string;
+  title: string;
+}[];
+type ApiDataType = {
+  display: number;
+  items: BookListType;
+  lastBuildDate: string;
+  start: number;
+  total: number;
+};
+
+const infiniteScroll = () => {
+  const [bookList, setBookList] = useState<BookListType>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [startNum, setStartNum] = useState<number>(1);
+
+  const callBookList = async () => {
+    await fetch(`http://localhost:5555/book/love?keyword=사랑&start=${startNum}`)
+      .then((res) => {
+        setIsLoading(true);
+        return res.json();
+      })
+      .then((data: ApiDataType) => {
+        setIsLoading(false);
+        console.log(data.items);
+        setBookList([...bookList, ...data.items]);
+        if (!data.items.length) {
+          setStartNum((prevState) => prevState + 10);
+        }
+      });
+  };
+  useEffect(() => {
+    callBookList();
+  }, []);
+  return (
+    <div>
+      <h1>사랑과 관련된 모든 책 모음</h1>
+      {React.Children.toArray(
+        bookList.map((item) => (
+          <div>
+            <h2>{item.title}</h2>
+          </div>
+        ))
+      )}
+      {isLoading ? <p>로딩 중...</p> : ''}
+    </div>
+  );
+};
 
 export default infiniteScroll;
