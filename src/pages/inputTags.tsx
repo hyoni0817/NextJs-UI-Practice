9214d7547e817b810d2985dfcd278e0ddb6b4345
hyoni0817 <hyoni0817@naver.com>
2022-10-16 18:12:42 +0900

    Update: 제목과 키워드 영역 크기 수정 및 중앙 정렬

--- src/pages/inputTags.tsx
+++ src/pages/inputTags.tsx
@@ -2,18 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 import KeywordInput from '../components/food/KeywordInput';
 
+const H1 = styled.h1`
+  text-align: center;
+`;
+
+const Container = styled.div`
+  width: 100&;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  flex-direction: column;
+`;
+
+const FoodKeywordBox = styled.div`
+  width: 100%;
+
+  @media screen and (min-width: 768px) {
+    width: 728px;
+  }
+`;
+
 const InputTags = () => {
   const handleSelectKeyword = (value: string[]) => {
     console.log({ value });
   };
 
   return (
-    <>
-      <h1>Food Keywords</h1>
-      <div>
+    <Container>
+      <FoodKeywordBox>
+        <H1>🍒Food Keywords🍒</H1>
         <KeywordInput onSelect={handleSelectKeyword} />
-      </div>
-    </>
+      </FoodKeywordBox>
+    </Container>
   );
 };
 
