9c04c460d505080f652e9c9ccf73997d7badb280
hyoni0817 <hyoni0817@naver.com>
2022-10-16 17:52:45 +0900

    Feat: 키워드를 입력하는 컴포넌트 영역 추가

--- src/pages/inputTags.tsx
+++ src/pages/inputTags.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import KeywordInput from '../components/food/KeywordInput';
 
-const InputTags = () => (
-  <>
-    <h1>Food Keywords</h1>
-    <div>키워드 입력 영역</div>
-  </>
-);
+const InputTags = () => {
+  const handleSelectKeyword = (value: string[]) => {
+    console.log({ value });
+  };
+
+  return (
+    <>
+      <h1>Food Keywords</h1>
+      <div>
+        <KeywordInput onSelect={handleSelectKeyword} />
+      </div>
+    </>
+  );
+};
 
 export default InputTags;
