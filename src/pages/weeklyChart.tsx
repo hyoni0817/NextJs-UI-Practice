b729f29c0c5099818f4892065cf9f15bc57897b5
hyoni0817 <hyoni0817@naver.com>
2023-04-09 21:05:54 +0900

    Update: area 차트로 변경 및 시리즈 추가, 데이터 라벨 제거

--- src/pages/weeklyChart.tsx
+++ src/pages/weeklyChart.tsx
@@ -9,6 +9,9 @@ const WeeklyChart = () => {
       chart: {
         id: 'apexchart-example',
       },
+      dataLabels: {
+        enabled: false,
+      },
       xaxis: {
         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
       },
@@ -18,10 +21,14 @@ const WeeklyChart = () => {
         name: 'series-1',
         data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
       },
+      {
+        name: 'series2',
+        data: [11, 32, 45, 32, 34, 52, 41, 80, 100],
+      },
     ],
   };
 
-  return <ReactApexChart options={data.options} series={data.series} type="bar" width={500} height={320} />;
+  return <ReactApexChart type="area" options={data.options} series={data.series} width={500} height={320} />;
 };
 
 export default WeeklyChart;
