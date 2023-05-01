49e96766c91049e5641b7e075883a32184ba1821
hyoni0817 <hyoni0817@naver.com>
2023-05-01 22:42:46 +0900

    Setting: next.config.js에 sentry 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+const withPlugins = require('next-compose-plugins');
+
+const moduleExports = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -7,3 +19,5 @@ module.exports = {
     return config;
   },
 };
+
+module.exports = withSentryConfig(withPlugins([], nextConfig), moduleExports, sentryWebpackPluginOptions);
